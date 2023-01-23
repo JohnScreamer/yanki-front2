ccc76fe85991de2b3b65adfe4c70ddab0003a745
JohnScreamer <bogaturscreamer@gmail.com>
2023-01-23 11:46:36 +0200

    first commit

--- pages/favorite/index.tsx
+++ pages/favorite/index.tsx
@@ -11,9 +11,8 @@ const favorite: FC<FavoriteType> = () => {
     const games = useAppSelector((state) => state.profile.favorite);
     const route = useRouter();
     const isWindow = typeof window === "undefined" ? false : true;
-    console.log(favorite.length);
 
-    if (!favorite.length && isWindow) {
+    if (!games.length && isWindow) {
         route.push("/");
     }
 
