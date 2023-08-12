4376389e30b959c3487cf43f58c0bb093469643d
Ivan Krychuk <91059278+JohnScreamer@users.noreply.github.com>
2023-08-12 22:57:51 +0300

    Update url.ts

--- common/url.ts
+++ common/url.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://yankisli.herokuapp.com"; //https://yankisli.herokuapp.com //http://localhost:4000
+export const BASE_URL = "https://johnscreamer-yanki-back.onrender.com"; //https://yankisli.herokuapp.com //http://localhost:4000
